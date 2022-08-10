a723211c768a146197a53d42ecbfb0eb46916448
johnny540 <kagitha40@gmail.com>
2022-08-10 20:21:45 +0530

    Spark_examples1.py is tha first spark job with pycharm IDE.

---
+++ spark_example1.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Spark Example 1').getOrCreate()
+
+list1 = [['Ram', 30], ['Anil', 28]]
+df = spark.createDataFrame(list1, ['name', 'age'])
+# df.show()
+# spark.stop()
+# LTI interview questions
+df1 = spark.createDataFrame([[3],[3],[3],[3]],['id'])
+df2 = spark.createDataFrame([[3],[None],[3]],['id'])
+# df1.show()
+# df2.show()
+df1.join(df2,['id'],'left').show(50,False)
+# Result
+"""
++---+
+|id |
++---+
+|3  |
+|3  |
+|3  |
+|3  |
+|3  |
+|3  |
+|3  |
+|3  |
++---+
+
+
+Process finished with exit code 0
+
+"""
